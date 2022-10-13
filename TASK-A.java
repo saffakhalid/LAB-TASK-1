69a861232d046fa269689848cff34792516b8d9c
SAFFA <115431012+pandaishere1742@users.noreply.github.com>
2022-10-13 10:02:20 -0700

    Add files via upload

---
+++ TASK-A.java
@@ -0,0 +1,22 @@
+ class Bajwa {
+
+    public static void main(String[]arg){
+        int v = 20;
+        System.out.println(v +" = this is an integer value");
+        double w = 121.6;
+        System.out.println(w +" = this is an Double value.");
+        // Float c = 1.0;
+        // System.out.println(c +" = this is an flouting point value");
+        boolean truefalse = true&false;
+        System.out.println(truefalse +" = this is an Boolean value.");
+        short x = -21;
+        System.out.println(x +" = this is an Short data type value."); 
+        String y = "Safaa" ;
+        System.out.println(y +" = this is an String value.");
+        char z = 'S';
+        System.out.println(z +" = this is an Charactor value.");
+
+
+    }
+
+}
